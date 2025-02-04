8029824b58b119cb0039aa3a5a0ac09128ba2f17
9susanta <rsusanta0@gmail.com>
2025-02-04 10:33:29 +0530

    #2 EF Core Set Up Done

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            public DbSet<Product> Products { get; set; }
+        }
+    }
+}
